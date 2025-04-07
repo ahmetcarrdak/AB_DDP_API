6f0b87f76ee19034e35c2f9a9ce6c25a7245577f
Ahmet Çardak <ahmetcardak@Cagris-MacBook-Pro.local>
2025-04-08 00:32:38 +0300

    V1

--- DDPApi/Program.cs
+++ DDPApi/Program.cs
@@ -97,11 +97,16 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowAllOrigins");
 app.UseHttpsRedirection();
